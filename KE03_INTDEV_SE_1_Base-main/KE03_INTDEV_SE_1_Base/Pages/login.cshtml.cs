40f5bd77e8dc3efa232964394be3e0841256e5bb
Eborm <bramwesel@gmail.com>
2025-05-21 18:09:49 +0200

    inloggen werkt nu en wordt opgeslagen in de session

--- KE03_INTDEV_SE_1_Base-main/KE03_INTDEV_SE_1_Base/Pages/login.cshtml.cs
+++ KE03_INTDEV_SE_1_Base-main/KE03_INTDEV_SE_1_Base/Pages/login.cshtml.cs
@@ -2,6 +2,7 @@ using DataAccessLayer.Interfaces;
 using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using KE03_INTDEV_SE_1_Base.Pages;
 
 namespace KE03_INTDEV_SE_1_Base.Pages
 {
@@ -18,10 +19,27 @@ namespace KE03_INTDEV_SE_1_Base.Pages
             _UserRepository = UserRepository;
             Users = new List<Customer>();
         }
-        public void OnGet()
+        public void OnGet(int? setId)
         {
             Users = _UserRepository.GetAllCustomers().ToList();
             _logger.LogInformation($"getting all {Users.Count} Users");
+
+            if (setId.HasValue)
+            {
+                HttpContext.Session.SetObjectAsJson("User_id", setId.Value);
+                _logger.LogInformation("User ID {UserId} stored in session via GET.", setId.Value);
+            }
+        }
+
+        public void OnPostSetAccount(int id)
+        {
+            HttpContext.Session.SetObjectAsJson("User_id", id);
+            _logger.LogInformation("User ID {UserId} stored in session.", id);
+        }
+
+        public int GetUserId()
+        {
+            return HttpContext.Session.GetObjectFromJson<int?>("User_id") ?? -1;
         }
     }
 }
--- KE03_INTDEV_SE_1_Base-main/KE03_INTDEV_SE_1_Base/Program.cs
+++ KE03_INTDEV_SE_1_Base-main/KE03_INTDEV_SE_1_Base/Program.cs
@@ -11,6 +11,16 @@ namespace KE03_INTDEV_SE_1_Base
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDistributedMemoryCache(); // Nodig voor sessie
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // bijv. 30 minuten
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            builder.Services.AddRazorPages();
+
+
             // We gebruiken voor nu even een SQLite voor de database,
             // omdat deze eenvoudig lokaal te gebruiken is en geen extra configuratie nodig heeft.
             builder.Services.AddDbContext<MatrixIncDbContext>(
@@ -48,6 +58,8 @@ namespace KE03_INTDEV_SE_1_Base
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseSession();
 
             app.UseAuthorization();
 
